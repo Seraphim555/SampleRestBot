69e6e2e08a5e7270a5f6437cb79b48f889b23876
Ryan Gosling <vjlev671games@gmail.com>
2024-12-02 01:02:46 +0500

    Dобавлены тестовые кнопки

--- src/main/java/com/SampleRestBot/SampleRestBot/service/TelegramBot.java
+++ src/main/java/com/SampleRestBot/SampleRestBot/service/TelegramBot.java
@@ -11,9 +11,13 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -105,6 +109,29 @@ public class TelegramBot extends TelegramLongPollingBot {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
+
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+
+        KeyboardRow row = new KeyboardRow();
+
+        row.add("Вика");
+        row.add("зацени");
+
+        keyboardRows.add(row);
+
+        row = new KeyboardRow();
+
+        row.add("какие");
+        row.add("кнопки");
+        row.add("смешные");
+
+        keyboardRows.add(row);
+
+        keyboardMarkup.setKeyboard(keyboardRows);
+
+        message.setReplyMarkup(keyboardMarkup);
 
         try{
             execute(message);
